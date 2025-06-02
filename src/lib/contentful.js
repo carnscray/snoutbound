ef3dc86da2642ed91476737cd27d145f7b876528
carnscray <bpggarth@gmail.com>
2025-06-02 23:18:51 +0000

    Pending changes exported from your codespace

---
+++ src/lib/contentful.js
@@ -0,0 +1,9 @@
+// src/lib/contentful.js
+import { createClient } from 'contentful';
+
+const client = createClient({
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.CONTENTFUL_ACCESS_TOKEN,
+});
+
+export default client;
